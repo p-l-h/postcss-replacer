ba7f897a21f56013fec71b40f4ac714e89ec527b
peilonghui <peilonghui@baijiahulian.com>
2018-10-08 17:47:29 +0800

    update repalce

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 var postcss = require('postcss');
+
 module.exports = postcss.plugin('postcss-replace', function (opts) {
     opts = opts || {};
     var mappers = opts.mappers || [];
 
-    // Work with options here
-
-    return function (root) {
-        if (!mappers.length) {
+    var replacer = function (rule, key) {
+        var value = rule[key];
+        if (!value) {
             return;
         }
-        root.walkAtRules('font-face', function (rule) {
-            rule.walkDecls('src', function (decl) {
-                var value = decl.value;
-                mappers.forEach(function (item) {
-                    value = value.replace(item.source, item.target);
-                });
-                decl.value = value;
-            });
+        mappers.forEach(function (item) {
+            rule[key] = value.replace(item.source, item.target);
+            value = rule[key];
         });
+    };
+
+    return function (root) {
+        if (!Object.keys(mappers).length) {
+            return false;
+        }
+        root.walk(function (rule) {
+            if (rule.type === 'atrule') {
+                replacer(rule, 'params');
+            } else if (rule.type === 'decl') {
+                replacer(rule, 'value');
+            }
+        });
+        return true;
     };
 });
--- index.test.js
+++ index.test.js
@@ -3,23 +3,24 @@ var postcss = require('postcss');
 var plugin = require('./');
 
 function run(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then(result => {
-            expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
-        });
+    return postcss([ plugin(opts) ]).process(input, {
+        from: undefined
+    }).then(result => {
+        expect(result.css).toEqual(output);
+        expect(result.warnings().length).toBe(0);
+    });
 }
 
 it('no input', function () {
     return run('', '', {});
 });
 
-it('replace abc to def',  function () {
-    return run('@font-face { src: abc }', '@font-face { src: def }', {
+it('repalce @import "a.css" to @import "b.scss"',  function () {
+    return run('@import "a.css"', '@import "b.scss"', {
         mappers: [
             {
-                source: 'abc',
-                target: 'def'
+                source: 'a.css',
+                target: 'b.scss'
             }
         ]
     });
